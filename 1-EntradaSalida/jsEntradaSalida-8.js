8855da2199b8ba2477f4ca23c3d087510fd300ba
alumno <alumno>
2019-02-08 21:41:40 -0300

    falta terminar ej 9

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,12 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+	var dividendo;
+    var divisor;
+    var resultado;
+dividendo = parseInt(document.getElementById("numeroDividendo").value);
+divisor = parseInt(document.getElementById("numeroDivisor").value);
+resultado = dividendo % divisor;
+
+alert("su resultado es " + resultado )
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,15 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+var sueldo;
+var resultado;
+var calculo;
+var calculo2;
 
+sueldo = parseInt(document.getElementById("sueldo").value);
+calculo = sueldo * 10 /100;
+calculo2 = sueldo + calculo;
+resultado = parseInt(document.getElementById("resultado")value + calculo2 );
+ 
 	
 }
