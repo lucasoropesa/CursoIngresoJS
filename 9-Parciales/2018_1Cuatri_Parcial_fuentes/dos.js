a5189e12a7bd15f2a9f7a30de198d4c029e37f93
alumno <alumno>
2019-02-13 19:23:26 -0300

    hasta ejercicio 2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,9 @@
 function mostrar()
 {
-  
+  var nombre
+  var localidad
+ 
+  nombre =document.getElementById("elNombre").value;
+  localidad =document.getElementById("laLocalidad").value;
+alert("usted se llama " + nombre + " y vive en "  + localidad );
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,14 @@
 
 function mostrar()
 {
+var largo
+var ancho
+var resultado
 
+largo =prompt("ingrese el largo ");
+ancho =prompt("ingrese el ancho");
+largo =parseInt(largo);
+ancho =parseInt(ancho);
+resultado =2*largo+2*ancho;
+alert("el perimetro es "+resultado);
 }
