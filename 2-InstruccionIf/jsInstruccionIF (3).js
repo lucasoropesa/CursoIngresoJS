5927245ae5f25a8f4de6415c03645182365b96fa
alumno <alumno>
2019-02-13 22:16:24 -0300

    hasta elinstruccion if4

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,15 @@
 function mostrar()
 {
 //tomo la edad  
-
-
+var edad
+edad = document.getElementById("edad").value;
+if(edad>=18)
+{
+alert("usted es mayor de edad");
+}
+edad = document.getElementById("edad").value;
+if(edad<18)
+{
+alert("usted es menor de edad");
+}
 }//FIN DE LA FUNCIÓN
